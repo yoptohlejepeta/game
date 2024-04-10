3e123b50c2521d21654538ee21482679928b0dfd
yoptohlejepeta <peta.kotlan@seznam.cz>
2024-04-10 11:15:59 +0200

    animated enemies

--- main.lua
+++ main.lua
@@ -2,16 +2,19 @@ local love = require("love")
 local math = require("math")
 local utils = require("utils")
 
-
 function beginContact(a, b, col)
     local aData = a:getUserData()
     local bData = b:getUserData()
 
     if aData == "player" and bData == "enemy" then
-        print("Player collided with enemy")
+        local playerBody = a:getBody()
+        local nx, ny = col:getNormal()
+        local impulseMagnitude = 5000
+
+        playerBody:applyLinearImpulse(-nx * impulseMagnitude, -ny * impulseMagnitude)
     end
-    
 end
+
 
 function love.load()
     -- disable blur
@@ -22,15 +25,13 @@ function love.load()
 
     enemies = {}
     player = {}
-    player.x = love.graphics.getWidth() / 2
-    player.y = love.graphics.getHeight() / 2
     player.width = 60
     player.height = 80
     player.speed = 200
     player.direction = "down"
     player.idle = true
-    player.body = love.physics.newBody(world, player.x - player.width / 2, player.y - player.height / 2, "dynamic")
-    player.shape = love.physics.newRectangleShape(0, 0, player.width, player.height)
+    player.body = love.physics.newBody(world, love.graphics.getWidth() / 2, love.graphics.getHeight() / 2, "dynamic")
+    player.shape = love.physics.newRectangleShape(player.width, player.height)
     player.fixture = love.physics.newFixture(player.body, player.shape)
     player.fixture:setUserData("player")
 
@@ -55,87 +56,75 @@ function love.load()
     player.currentFrame = 1
     player.animationSpeed = 0.1
     player.animationTimer = player.animationSpeed
+
+    -- Initialize enemies
+    for i = 1, 5 do
+        enemies[i] = {}
+        enemies[i].width = 60
+        enemies[i].height = 60
+        enemies[i].speed = math.random(50, 150)
+        enemies[i].body = love.physics.newBody(world, utils.random_choice(0, love.graphics.getWidth()), math.random(0, love.graphics.getHeight()), "dynamic")
+        enemies[i].shape = love.physics.newRectangleShape(enemies[i].width, enemies[i].height)
+        enemies[i].fixture = love.physics.newFixture(enemies[i].body, enemies[i].shape)
+        enemies[i].fixture:setUserData("enemy")
+
+        enemies[i].spriteSheet = love.graphics.newImage("sprites/demon_shug.png")
+        demonFrameWidth = enemies[i].spriteSheet:getWidth()
+        demonFrameHeight = enemies[i].spriteSheet:getHeight() / 4
+
+        enemies[i].quads = {}
+        for row = 1, 4 do
+            local x = 0
+            local y = (row - 1) * demonFrameHeight
+            enemies[i].quads[row] = love.graphics.newQuad(x, y, demonFrameWidth, demonFrameHeight, enemies[i].spriteSheet:getDimensions())
+    
+        end
+
+    end
 end
 
 function love.update(dt)
     world:update(dt)
-    player.body:setPosition(player.x, player.y)
-    if #enemies == 0 then
-        -- local number_of_enemies = math.random(1, 5)
-        for i = 1, 5 do
-            enemies[i] = {}
-            enemies[i].width = 60
-            enemies[i].height = 80
-            enemies[i].x = utils.random_choice(0, love.graphics.getWidth())
-            enemies[i].y = math.random(0, love.graphics.getHeight())
-            enemies[i].speed = math.random(50, 150)
-            enemies[i].body = love.physics.newBody(world, enemies[i].x, enemies[i].y, "dynamic")
-            enemies[i].shape = love.physics.newRectangleShape(0, 0, enemies[i].width, enemies[i].height)
-            enemies[i].fixture = love.physics.newFixture(enemies[i].body, enemies[i].shape)
-            enemies[i].fixture:setUserData("enemy")
-        end
-    else
-        for i = 1, #enemies do
-            local vector = {x = enemies[i].x - player.x, y = enemies[i].y - player.y}
-            local length = math.sqrt(vector.x^2 + vector.y^2)
-            vector.x = vector.x / length
-            vector.y = vector.y / length
-            enemies[i].x = enemies[i].x - vector.x * enemies[i].speed * dt
-            enemies[i].y = enemies[i].y - vector.y * enemies[i].speed * dt
-            enemies[i].body:setPosition(enemies[i].x, enemies[i].y)
-        end
-    end
+
     local moveX, moveY = 0, 0
 
     if love.keyboard.isDown("right") then
-      moveX = moveX + 1
-      player.direction = "right"
-      player.idle = false
-  end
-  if love.keyboard.isDown("left") then
-      moveX = moveX - 1
-      player.direction = "left"
-      player.idle = false
-  end
-  if love.keyboard.isDown("down") then
-      moveY = moveY + 1
-      player.direction = "down"
-      player.idle = false
-  end
-  if love.keyboard.isDown("up") then
-      moveY = moveY - 1
-      player.direction = "up"
-      player.idle = false
-  end
-  if love.keyboard.isDown("up") and love.keyboard.isDown("right") then
-      moveX = moveX + 1
-      moveY = moveY - 1
-      player.direction = "up_right"
-      player.idle = false
-  end
-  if love.keyboard.isDown("up") and love.keyboard.isDown("left") then
-      moveX = moveX - 1
-      moveY = moveY - 1
-      player.direction = "up_left"
-      player.idle = false
-  end
-  if love.keyboard.isDown("down") and love.keyboard.isDown("right") then
-      moveX = moveX + 1
-      moveY = moveY + 1
-      player.direction = "down_right"
-      player.idle = false
-  end
-  if love.keyboard.isDown("down") and love.keyboard.isDown("left") then
-      moveX = moveX - 1
-      moveY = moveY + 1
-      player.direction = "down_left"
-      player.idle = false
-  end
-  
-
-    if moveX == 0 and moveY == 0 then
-        player.idle = true
+        moveX = moveX + 1
+        player.direction = "right"
+        player.idle = false
     end
+    if love.keyboard.isDown("left") then
+        moveX = moveX - 1
+        player.direction = "left"
+        player.idle = false
+    end
+    if love.keyboard.isDown("down") then
+        moveY = moveY + 1
+        player.direction = "down"
+        player.idle = false
+    end
+    if love.keyboard.isDown("up") then
+        moveY = moveY - 1
+        player.direction = "up"
+        player.idle = false
+    end
+    if love.keyboard.isDown("up") and love.keyboard.isDown("right") then
+        player.direction = "up_right"
+        player.idle = false
+    end
+    if love.keyboard.isDown("up") and love.keyboard.isDown("left") then
+        player.direction = "up_left"
+        player.idle = false
+    end
+    if love.keyboard.isDown("down") and love.keyboard.isDown("right") then
+        player.direction = "down_right"
+        player.idle = false
+    end
+    if love.keyboard.isDown("down") and love.keyboard.isDown("left") then
+        player.direction = "down_left"
+        player.idle = false
+    end
+
 
     local length = math.sqrt(moveX^2 + moveY^2)
     if length ~= 0 then
@@ -143,8 +132,7 @@ function love.update(dt)
         moveY = moveY / length
     end
 
-    player.x = player.x + moveX * player.speed * dt
-    player.y = player.y + moveY * player.speed * dt
+    player.body:setLinearVelocity(moveX * player.speed, moveY * player.speed)
 
     if player.idle then
         player.currentFrame = 1
@@ -155,6 +143,26 @@ function love.update(dt)
             player.animationTimer = player.animationSpeed
         end
     end
+
+    for i, enemy in ipairs(enemies) do
+        local playerX, playerY = player.body:getPosition()
+        local enemyX, enemyY = enemy.body:getPosition()
+        local vector = {x = enemyX - playerX, y = enemyY - playerY}
+        local length = math.sqrt(vector.x^2 + vector.y^2)
+        vector.x = vector.x / length
+        vector.y = vector.y / length
+        enemy.body:setLinearVelocity(-vector.x * enemy.speed, -vector.y * enemy.speed)
+
+        if vector.x > 0 and math.abs(vector.x) > math.abs(vector.y) then
+            enemy.row = 3
+        elseif vector.y < 0 and math.abs(vector.y) > math.abs(vector.x) then
+            enemy.row = 1
+        elseif vector.y > 0 and math.abs(vector.y) > math.abs(vector.x) then
+            enemy.row = 4
+        elseif vector.x < 0 and math.abs(vector.x) > math.abs(vector.y) then
+            enemy.row = 2
+        end
+    end
 end
 
 function love.draw()
@@ -162,37 +170,51 @@ function love.draw()
     if player.direction == "up" then
         row = 5
     elseif player.direction == "down" then
-      row = 1
+        row = 1
     elseif player.direction == "left" then
-      row = 7
+        row = 7
     elseif player.direction == "right" then
-      row = 3
+        row = 3
     elseif player.direction == "up_right" then
-      row = 4
+        row = 4
     elseif player.direction == "up_left" then
-      row = 6
+        row = 6
     elseif player.direction == "down_right" then
-      row = 2
+        row = 2
     elseif player.direction == "down_left" then
-      row = 8
+        row = 8
     end
 
-    for i = 1, #enemies do
-        love.graphics.setColor(1, 0, 0)
-        love.graphics.rectangle("fill", enemies[i].x - enemies[i].width / 2, enemies[i].y - enemies[i].height / 2, enemies[i].width, enemies[i].height)
-    end
 
     love.graphics.setColor(1, 1, 1)
     love.graphics.setBackgroundColor(0.5, 0.5, 0.5)
     local scaleFactor = 5
 
-    local drawX = player.x - (frameWidth / 2) * scaleFactor
-    local drawY = player.y - (frameHeight / 2) * scaleFactor
+    local drawX, drawY = player.body:getPosition()
+
+    drawX = drawX - (frameWidth / 2) * scaleFactor
+    drawY = drawY - (frameHeight / 2) * scaleFactor
 
     love.graphics.draw(player.spriteSheet, player.quads[row][player.currentFrame], drawX, drawY, 0, scaleFactor)
-
-    -- love.graphics.circle("fill", player.x, player.y, 5)
     -- draw player hitboxes
     love.graphics.setColor(1, 0, 0)
-    love.graphics.rectangle("line", player.x - player.width / 2, player.y - player.height / 2, player.width, player.height)
+    local playerX, playerY = player.body:getPosition()
+    love.graphics.rectangle("line", playerX - player.width / 2, playerY - player.height / 2, player.width, player.height)
+
+    for i, enemy in ipairs(enemies) do
+        local playerX, playerY = player.body:getPosition()
+        local enemyX, enemyY = enemy.body:getPosition()
+        love.graphics.rectangle("line", enemyX - enemy.width / 2, enemyY - enemy.height / 2, enemy.width, enemy.height)
+        local vector = {x = enemyX - playerX, y = enemyY - playerY}
+        local length = math.sqrt(vector.x^2 + vector.y^2)
+        vector.x = vector.x / length
+        vector.y = vector.y / length
+        enemy.body:setLinearVelocity(-vector.x * enemy.speed, -vector.y * enemy.speed)
+    
+        -- Set demon_row to 1 for all enemies to draw the first row of the sprite sheet
+        -- Draw the demon (using the first row of the sprite sheet)
+        -- love.graphics.draw(enemy.spriteSheet, enemyX, enemyY, 0, scaleFactor)
+        love.graphics.draw(enemy.spriteSheet, enemy.quads[enemy.row], enemyX - enemy.width / 2, enemyY - enemy.height / 2, 0, 2)
+    end
+    
 end
